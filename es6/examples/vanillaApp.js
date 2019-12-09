17ac790153f90aa9dee98150e52a2c011126ec1e
James Smith <james.smith@djalbat.com>
2019-12-09 15:48:49 +0000

    Tided up a little.

--- es6/examples/vanillaApp.js
+++ es6/examples/vanillaApp.js
@@ -3,76 +3,85 @@
 const React = require('../react'),
       ReactDOM = require('../reactDOM');
 
+const Comment = React.createClass({
+  render: function() {
+    return (
+
+      <div className="comment">
+        <p>
+          {this.props.message}
+        </p>
+      </div>
+
+    );
+  },
+
+  componentDidMount: function() {
+    const message = this.props.message;
+
+    console.log('Comment mounted with message: ' + message)
+  },
+
+  componentWillUnmount: function() {
+    const message = this.props.message;
+
+    console.log('Comment unmounted with message: ' + message)
+  }
+});
+
+const CommentsList = React.createClass({
+  getInitialState() {
+    const messages = [
+            'Hello, world!',
+            'Hello world again...'
+          ],
+          state = {
+            messages
+          };
+
+    return state;
+  },
+
+  componentDidMount: function() {
+    console.log('Comments list mounted.')
+  },
+
+  render: function() {
+    const state = this.getState(),
+          { messages } = state,
+          comments = messages.map((message) =>
+
+            <Comment message={message} />
+
+          );
+
+    return (
+
+      <div className="comments-list">
+        {comments}
+      </div>
+
+    );
+  }
+});
+
 const vanillaApp = () => {
-  const rootDOMElement = document.getElementById('root');
+  const commentsList =
 
-  const Comment = React.createClass({
-    render: function() {
-      return (
+          <CommentsList />
 
-        <div className="comment">
-          <p>
-            {this.props.message}
-          </p>
-        </div>
+        ,
+        rootDOMElement = document.getElementById('root');
 
-      );
-    },
 
-    componentDidMount: function() {
-      const message = this.props.message;
-
-      console.log('comment mounted with message ' + message)
-    },
-
-    componentWillUnmount: function() {
-      const message = this.props.message;
-
-      console.log('comment unmounted with message ' + message)
-    }
-  });
-
-  const CommentsList = React.createClass({
-    getInitialState() {
-      const messages = [
-              "Hello, world!",
-              "Hello world again..."
-            ],
-            state = {
-              messages
-            };
-
-      return state;
-    },
-
-    render: function() {
-      const state = this.getState(),
-            { messages } = state,
-            comments = messages.map(function(message) {
-              return <Comment message={message} />;
-            });
-
-      return (
-
-        <div className="commentsList">
-          {comments}
-        </div>
-
-      );
-    },
-
-    componentDidMount: function() {
-      console.log('comments list mounted')
-    }
-  });
-
-  const commentsList = <CommentsList />;
-
-  ReactDOM.render(commentsList, rootDOMElement);
+  ReactDOM.render(
+    commentsList,
+    rootDOMElement
+  );
 
   setTimeout(function() {
     const messages = [
-            "Hello world yet again!!!"
+            'Hello world yet again!!!'
           ],
           state = {
             messages
