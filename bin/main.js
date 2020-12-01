67407abe8d41f099573e0dc044e951a165ca05a8
James Smith <james.smith@djalbat.com>
2020-12-01 21:19:52 +0000

    Improved the live reloading.

--- bin/main.js
+++ bin/main.js
@@ -1,10 +1,16 @@
 "use strict";
 
-const express = require("express");
+const lively = require("lively-cli"), ///
+      express = require("express");
+
+const { createReloadHandler } = lively;
 
 const server = express(), ///
-      staticRouter = express.static(".");
+      staticRouter = express.static("."),
+      reloadHandler = createReloadHandler("./examples.js");
 
 server.use(staticRouter);
+
+server.get("/reload", reloadHandler);
 
 server.listen(8888);
