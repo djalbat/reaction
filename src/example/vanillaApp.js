abff0209119e1c227bb6c78054a72d0eda24006d
James Smith <james.smith@djalbat.com>
2026-05-13 15:42:06 +0100

    A tiny change to the example.

--- src/example/vanillaApp.js
+++ src/example/vanillaApp.js
@@ -4,7 +4,7 @@ import { React, ReactDOM } from "../index"; ///
 
 import CommentsList from "./vanillaApp/commentsList";
 
-import { ROOT, DELAY } from "./vanillaApp/constants";
+import { ROOT, MESSAGES_DELAY } from "./vanillaApp/constants";
 
 export default function vanillaApp() {
   const commentsList =
@@ -19,6 +19,8 @@ export default function vanillaApp() {
     rootDOMElement
   );
 
+  const delay = MESSAGES_DELAY;
+
   setTimeout(() => {
     const messages = [
             "Hello world yet again!!!"
@@ -28,5 +30,5 @@ export default function vanillaApp() {
           };
 
     commentsList.setState(state);
-  }, DELAY);
+  }, delay);
 };
